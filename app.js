b5a0d5e2d63d5acb6c10c4db10efee3f4e3665d7
768733154 <768733154@qq.com>
2026-05-08 10:17:14 +0800

    feat: initialize mini games app with tetris, snake, gomoku, and 2048

---
+++ app.js
@@ -0,0 +1,11 @@
+App({
+  onLaunch() {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上基础库以使用云能力')
+      return
+    }
+    wx.cloud.init({
+      traceUser: true
+    })
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,120 @@
+const { questionBank } = require('../../utils/questions')
+
+function shuffle(arr) {
+  const copy = [...arr]
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[copy[i], copy[j]] = [copy[j], copy[i]]
+  }
+  return copy
+}
+
+Page({
+  data: {
+    total: 5,
+    current: 1,
+    score: 0,
+    timeLeft: 60,
+    selected: -1,
+    answered: false,
+    gameOver: false,
+    question: null,
+    message: ''
+  },
+
+  onLoad() {
+    this.startGame()
+  },
+
+  onUnload() {
+    this.clearTimer()
+  },
+
+  startGame() {
+    const selectedQuestions = shuffle(questionBank).slice(0, 5)
+    this.questions = selectedQuestions
+    this.qIndex = 0
+
+    this.setData({
+      total: selectedQuestions.length,
+      current: 1,
+      score: 0,
+      timeLeft: 60,
+      selected: -1,
+      answered: false,
+      gameOver: false,
+      message: '',
+      question: selectedQuestions[0]
+    })
+
+    this.startTimer()
+  },
+
+  startTimer() {
+    this.clearTimer()
+    this.timer = setInterval(() => {
+      const next = this.data.timeLeft - 1
+      if (next <= 0) {
+        this.setData({ timeLeft: 0 })
+        this.finishGame('时间到！')
+        return
+      }
+      this.setData({ timeLeft: next })
+    }, 1000)
+  },
+
+  clearTimer() {
+    if (this.timer) {
+      clearInterval(this.timer)
+      this.timer = null
+    }
+  },
+
+  chooseOption(e) {
+    if (this.data.answered || this.data.gameOver) return
+
+    const selected = Number(e.currentTarget.dataset.index)
+    const correct = this.data.question.answer
+    const isRight = selected === correct
+
+    this.setData({
+      selected,
+      answered: true,
+      score: isRight ? this.data.score + 20 : this.data.score,
+      message: isRight
+        ? '回答正确，气势如虹！'
+        : `回答错误。${this.data.question.explain}`
+    })
+  },
+
+  nextQuestion() {
+    if (!this.data.answered || this.data.gameOver) return
+
+    const nextIndex = this.qIndex + 1
+    if (nextIndex >= this.questions.length) {
+      this.finishGame('闯关完成！')
+      return
+    }
+
+    this.qIndex = nextIndex
+    this.setData({
+      current: nextIndex + 1,
+      question: this.questions[nextIndex],
+      selected: -1,
+      answered: false,
+      message: ''
+    })
+  },
+
+  finishGame(tip) {
+    this.clearTimer()
+    this.setData({
+      gameOver: true,
+      message: `${tip} 最终得分：${this.data.score}`
+    })
+  },
+
+  restart() {
+    this.startGame()
+  }
+})
